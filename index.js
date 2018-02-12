15c4cfb4ff32fef304d904c13fc29332c8a9a3b4
phapdinh <centralparkloop@yahoo.com>
2018-02-11 19:28:58 -0500

    added some middleware

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger'
+import thunkMiddleware from 'redux-thunk'
 import todoApp from './reducers';
 import App from './pages/app';
 
-let store = createStore(todoApp)
+let store = createStore(todoApp, applyMiddleware(logger, thunkMiddleware));
 
 render(
   <Provider store={store}>
