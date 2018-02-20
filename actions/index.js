302ec6af36705419906cd5d88816ace2e23ad864
phapdinh <centralparkloop@yahoo.com>
2018-02-20 14:03:53 -0500

    ignore debug files and remove unnnecesary paren

--- actions/index.js
+++ actions/index.js
@@ -1,5 +1,5 @@
 let nextTodoId = 0;
-export const addTodo = text => (dispatch) => dispatch({
+export const addTodo = text => dispatch => dispatch({
     type: 'ADD_TODO',
     id: nextTodoId++,
     text
