359270ac1c3ef1e5da6032ab91eb807840850cf2
phapdinh <centralparkloop@yahoo.com>
2018-02-22 13:58:33 -0500

    added react hot loader

--- index.js
+++ index.js
@@ -2,16 +2,24 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger'
-import thunkMiddleware from 'redux-thunk'
+import logger from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+import { AppContainer } from 'react-hot-loader';
 import todoApp from './reducers';
 import App from './pages/app';
 
 let store = createStore(todoApp, applyMiddleware(logger, thunkMiddleware));
 
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('app')
+    <Provider store={store}>
+        <AppContainer>
+            <App />
+        </AppContainer>
+    </Provider>,
+    document.getElementById('app')
 )
+
+// Webpack Hot Module Replacement API
+if (module.hot) {
+    module.hot.accept('./pages/app', () => { render(App) })
+}
